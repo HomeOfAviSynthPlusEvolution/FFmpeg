06cf949f61d89ae61dca1199eeb8f582c9631620
akarin <i@akarin.info>
2021-03-25 06:42:15 -0400

    3rdparty/qaac: import CoreAudio and AudioToolbox headers from qaac
    URL: https://github.com/nu774/qaac
    License: Public domain
    Revision: 4cb8b54ea4339eafb07149a2224ecab4b4c0db6a
    
    Signed-off-by: akarin <i@akarin.info>

---
+++ 3rdparty/qaac/AudioToolbox/AudioToolbox.h
@@ -0,0 +1,13 @@
+#ifndef CoreAudioToolbox_H
+#define CoreAudioToolbox_H
+
+#include "../CoreAudio/MacTypes.h"
+#include "../CoreAudio/CoreFoundation.h"
+#include "../CoreAudio/CoreAudioTypes.h"
+#include "../CoreAudio/AudioComponent.h"
+#include "../CoreAudio/AudioCodec.h"
+#include "../CoreAudio/AudioConverter.h"
+#include "../CoreAudio/AudioFile.h"
+#include "../CoreAudio/AudioFormat.h"
+
+#endif
